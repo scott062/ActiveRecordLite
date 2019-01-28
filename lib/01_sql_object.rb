a50f937a95177ba644eed5286d975bb81c693212
scott <scott.m.burdick@gmail.com>
2019-01-28 11:35:10 -0500

    Complete initialize method

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -49,7 +49,14 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      if self.class.columns.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
   end
 
   def attributes
