398e3cb75beb32041c3efd3c57fdeb1be53c5f4e
scott <scott.m.burdick@gmail.com>
2019-02-01 17:59:29 -0500

    Finish searchable module and begin associatable module

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,21 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map { |key| "#{key} = ?"}.join(" AND ")
+    args = params.values
+    results = DBConnection.execute(<<-SQL, *args)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -14,13 +14,21 @@ class AssocOptions
   end
 
   def table_name
-    # ...
+
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      :primary_key => :id,
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
